b5d0ed655bcca9bb87b029a85c920ca05bc2ef44
Kujtim.Prenku <Kujtim.Prenku@sqa-consulting.com>
2022-05-17 13:15:15 +0200

    Changed value of 'global' variable in vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    "global": {},
+    "global": "window",
     "process.env.NODE_DEBUG": JSON.stringify(""),
   },
 })
